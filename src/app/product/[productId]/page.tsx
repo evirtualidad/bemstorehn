849dbce12861f301ddc3f92aef811c3d87a1c5f1
Erick Garcia <evirtualidad@gmail.com>
2025-07-19 15:31:49 +0000

    sigue igual, hagamos lo siguente crea un header fijo para estos botones

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -69,7 +69,7 @@ export default function ProductDetailPage({ params }: ProductDetailPageProps) {
       <ProductHeader />
       
       {/* Scrollable Content */}
-      <div className="pb-24">
+      <div className="overflow-y-auto pb-24">
         {/* Product Image Section */}
         <div className="relative w-full aspect-square flex-shrink-0">
           <Image
