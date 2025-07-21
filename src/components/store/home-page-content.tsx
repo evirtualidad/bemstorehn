1a8449e0017e2abcfb2e9f0cf2bd6571135211fe
Erick Garcia <evirtualidad@gmail.com>
2025-07-21 18:47:19 +0000

    no, era el fondo de todo, y la barra de busqueda un gris para que se dis

--- src/components/store/home-page-content.tsx
+++ src/components/store/home-page-content.tsx
@@ -97,7 +97,7 @@ export function HomePageContent({
                     <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
                     <Input 
                         placeholder="Search..." 
-                        className="bg-card rounded-full h-14 pl-12 text-base border-none" 
+                        className="bg-secondary rounded-full h-14 pl-12 text-base border-none" 
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
                     />
