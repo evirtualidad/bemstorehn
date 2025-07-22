324ec911eea710d43277cd907114dbf29ba95dfe
Erick Garcia <evirtualidad@gmail.com>
2025-07-22 02:46:10 +0000

    estoy visualizando en otro navegador y en ligar de L en precio aparece H

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number, currencyCode: string, maximumFractionDigits: number = 2) {
+  const numberFormatter = new Intl.NumberFormat('es-HN', {
+    maximumFractionDigits: maximumFractionDigits,
+    minimumFractionDigits: 2,
+  });
+  
+  // Custom logic to always display "L"
+  if (currencyCode === 'HNL') {
+    return `L. ${numberFormatter.format(amount)}`;
+  }
+
+  // Fallback to default for other currencies
   return new Intl.NumberFormat('es-HN', {
     style: 'currency',
     currency: currencyCode,
